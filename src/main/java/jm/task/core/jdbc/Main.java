3c7af091f12470b0da1c03ee692e3332454144ae
Ilnur Badro <101541200+ibadro@users.noreply.github.com>
2022-09-27 16:29:12 +0500

    !

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
+
+        UserService userServiceImpl = new UserServiceImpl();
+
+        userServiceImpl.createUsersTable();
+
+        userServiceImpl.saveUser("ilnur","badro", (byte) 40);
+        userServiceImpl.saveUser("Daniel","badro", (byte) 12);
+        userServiceImpl.saveUser("Damir","badro", (byte) 8);
+        userServiceImpl.saveUser("XXXX01","badro", (byte) 40);
+
+        userServiceImpl.removeUserById(3);
+        userServiceImpl.getAllUsers();
+        userServiceImpl.cleanUsersTable();
+        userServiceImpl.dropUsersTable();
+
+
+
+
         // реализуйте алгоритм здесь
     }
 }
